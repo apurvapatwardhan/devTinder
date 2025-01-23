b1c3f5d74884d0897f3ca5155596b39d3e0ffff9
apurvapatwardhan <avp2798@gmail.com>
2025-01-23 12:27:53 +0530

    exploring validation and data sanitisation

--- src/app.js
+++ src/app.js
@@ -8,14 +8,71 @@ app.use(express.json());
 
 app.post("/signup", async (req, res, next) => {
   const user = new User(req.body);
-
   try {
     await user.save();
     res.send("Data added");
   } catch (err) {
-    res.status(500).send("Something wen wrong");
+    res.status(500).send(err.message);
   }
 });
+
+//GET user by email
+app.get("/user", async (req, res) => {
+  const email = req.body.email;
+  try {
+    const user = await User.find({ email });
+    if(user.length === 0) {
+        throw new Error("User doesnt exist")
+    }
+    res.send(user[0]);
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+// GET /feed
+app.get("/feed",async (req, res, next) => {
+
+    try {
+        const users = await User.find({});
+        res.send(users)
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: err.message,
+          });
+    }
+});
+
+app.delete("/user", async (req, res) => {
+    const userId = req.body.userId;
+    try {
+        await User.findByIdAndDelete(userId);
+        res.send("User deleted success");
+    } catch (error) {
+        res.status(500).send("Something went wrong")
+    }
+})
+
+app.patch("/user:userId", async (req, res) => {
+    const body = req.body;
+    const userId = req.params.userId;
+    try {
+        const ALLOWED_UPDATES = ['gender', 'age', 'skills', 'photoUrl', 'about'];
+        const isUpdateAllowed = Object.keys(body).every(key => ALLOWED_UPDATES.includes(key));
+        if(!isUpdateAllowed) {
+          throw new Error("Update not allowed");
+        }
+        const updatedUser = await User.findByIdAndUpdate(user.userId, user, {returnDocument: 'after', runValidators: true});
+        console.log(updatedUser)
+        res.send("User Updated success");
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
 
 connectToDB()
   .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,28 +1,81 @@
 
 
 const {Schema, model} = require("mongoose");
+const validator = require('validator')
 
 
 const userSchema = new Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 4,
+        maxLength: 20
     },
     lastName: {
-        type: String
+        type: String,
+        maxLength: 20,
+        minLength: 1
     },
     email: {
-        type: String
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        maxLength: 20,
+        trim: true,
+        validate: {
+            validator: (value) => validator.isEmail(value),
+            message: (props) => `Not a valid email`
+        }
     },
     password: {
-        type: String
+        type: String,
+        required: true,
+        maxLength: 40,
+        validate: {
+            validator: (value) => validator.isStrongPassword(value),
+            message: "Enter a String password!!"
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18,
+        max: 70
     },
     gender: {
-        type: String
+        type: String,
+        validate: {
+            validator: (value) => {
+                if(!['male', 'female', 'other'].includes(value)) {
+                    return false
+                }
+                return true
+            },
+            message: (props) => `${props.value} should be either male or female`
+        }
+    },
+    photoUrl: {
+        type: String,
+        maxLength:100,
+        minLength: 6,
+        validate: {
+            validator: (value) => validator.isURL(value),
+            message: () => "Not a valid url"
+        }
+        },
+    about: {
+        type: String,
+        default: "this is a default description of user",
+        maxLength: 100
+    },
+    skills: {
+        type: [String],
+        validate: {
+            validator: (value) => value.length <= 10,
+            message: (props) => `Maximum skills can be 10`
+        }
     }
-})
+}, {timestamps: true})
 
 const User = model("User", userSchema);
 
